b842f3ccfd55ef015e383194f7948036a497c7c3
Tabish Bashir <tabishbashir@gmail.com>
2018-05-06 14:37:10 +0530

    Add jquery module and some project functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import 'pubsub-js';
+import $ from "jquery";
+global.$ = global.jQuery = $;
+import {Render} from '../dist/render.js'
+import {Projects,Project,ToDo} from '../dist/ProjectFunctionality.js'
