3f970b15ff73488098e3d5ca7fb389a979515f4f
Tabish Bashir <tabishbashir@gmail.com>
2018-05-07 11:22:24 +0530

    Add project card and project entry functionality

--- dist/render.js
+++ dist/render.js
@@ -1,14 +1,51 @@
 const Render = ( () => {
 
-    const viewTodoCard = () => {
-      $(".Todo-card").css('display','grid');
-    }
+    const viewTodoCard = (() => {
+      $('.new-task-btn').click(() => {
+        $(".Todo-card").css('display','grid');
+      })
+    })();
 
-    const closeTodoCard = () => {
-      $(".Todo-card").css('display','none');
-    }
+    const closeTodoCard = (() => {
+      $('.close-todo-card').click(() => {
+        $(".Todo-card").css('display','none');
+      })
+    })();
 
-    return {viewTodoCard,closeTodoCard}
+    const viewProjectCard = (() => {
+      $('.new-project-btn').click(() => {
+        $(".new-project-card").css('display','grid');
+      })
+    })();
+
+    const closeProjectCard = (() => {
+      $('.close-project-card').click(() => {
+        $(".new-project-card").css('display','none');
+      })
+    })();
+
+    const viewProjects = (project) => {
+      let p_name = project.getName();
+      $(".sidebar").append(`<div data-id="${p_name}" class="project"><p
+                              class="${p_name}">${p_name}</p></div>`);
+      viewTodos();
+    };
+
+    const viewTodos = () => {
+      const viewOnSidebar = (() => {
+        $('.project p').each(function(){
+          $(this).click(()=>{
+            let name = $(this).attr('class');
+            let project = $(this.parentElement).data('id');
+            Object.keys(Projects.getProjects()[project].getTodos()).forEach(function(){
+              console.log(this);
+            })
+          })
+        })
+      })();
+    };
+
+    return {viewProjects};
 } )()
 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import 'pubsub-js';
-import $ from "jquery";
-global.$ = global.jQuery = $;
-import {Render} from '../dist/render.js'
-import {Projects,Project,ToDo} from '../dist/ProjectFunctionality.js'
+import {Projects,Project,ToDo} from '../dist/ProjectFunctionality.js';
+import {Render} from '../dist/render.js';
+
+const todoApp = (() => {
+
+    const addProject = (() => {
+      $('.new-project-card button').click(() => {
+        let project_name = $('.new-project-card input').val();
+        let project = Project(project_name);
+        Projects.addProject(project_name,project);
+        Render.viewProjects(project);
+      })
+    })();
+
+    return {};
+})();
