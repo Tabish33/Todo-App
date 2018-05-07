e75168adc339a8e2098b068339cf957f04652d8c
Tabish Bashir <tabishbashir@gmail.com>
2018-05-07 21:00:02 +0530

    Add viewing of todos on switching project and on adding todo

--- dist/render.js
+++ dist/render.js
@@ -3,6 +3,7 @@ const Render = ( () => {
     const viewTodoCard = (() => {
       $('.new-task-btn').click(() => {
         $(".Todo-card").css('display','grid');
+        $(".new-project-card").css('display','none');
       })
     })();
 
@@ -15,6 +16,7 @@ const Render = ( () => {
     const viewProjectCard = (() => {
       $('.new-project-btn').click(() => {
         $(".new-project-card").css('display','grid');
+        $(".Todo-card").css('display','none');
       })
     })();
 
@@ -34,13 +36,53 @@ const Render = ( () => {
 
     })();
 
+    const AddTodosToProjectArea = () =>{}
+
     const viewTodos = (() => {
-      const viewOnSidebar = (() => {
-        PubSub.subscribe("add todo",(msg,data) => {
-          let project_name = data[0] ,  todo_name = data[1];
-          $(`.${project_name}`).append(`<li>${todo_name}</li>`);
-        })
-      })();
+
+        const viewOnSidebar = (() => {
+          PubSub.subscribe("add todo",(msg,data) => {
+            let project_name = data[0] ,  todo_name = data[1];
+            $(`.${project_name}`).append(`<li>${todo_name}</li>`);
+          })
+        })();
+
+        const viewOnProjectArea = (() => {
+
+            const onProjectChange = (() => {
+                PubSub.subscribe("current project", (msg, curr_project_obj) => {
+                  $(".project-cards").empty();
+                  let todos = curr_project_obj.getTodos() ;
+                  for (let todo in todos){
+                    let title = todos[todo].getTitle();
+                    let description = todos[todo].getDescription();
+                    $(".project-cards").append(`
+                      <div class="note">
+                          <input type="text"  placeholder="Title" value=${title}>
+                          <textarea placeholder="Description">${description}</textarea>
+                      </div>
+                      `)
+                  }
+                })
+            })();
+
+            const onNewProject = (() => {
+                PubSub.subscribe("add project", () => { $(".project-cards").empty(); })
+            })()
+
+            const onNewTodo = (() => {
+                PubSub.subscribe("add todo",(msg,data)=> {
+                  let todoObj = data[2];
+                  $(".project-cards").append(`
+                    <div class="note">
+                        <input type="text"  placeholder="Title" value=${todoObj.getTitle()}>
+                        <textarea placeholder="Description">${todoObj.getDescription()}</textarea>
+                    </div>
+                    `)
+                })
+            })();
+
+        })();
     })();
 
     return {};
--- src/index.js
+++ src/index.js
@@ -8,25 +8,9 @@ const todoApp = (() => {
       $('.new-project-card button').click(() => {
         let project_name = $('.new-project-card input').val();
         let project = Project(project_name);
-        Projects.addProject(project_name,project);
+        getProjectsObj().addProject(project_name,project);
       })
     })();
-
-    const setCurrentProject = (() => {
-      PubSub.subscribe('project added', () => {
-        let projects = document.querySelectorAll(".project p");
-        projects.forEach((project) => {
-          project.onclick = (e) => {
-            let curr_proj = e.currentTarget.innerHTML;
-            Projects.setCurrentProject(curr_proj);
-          }
-        })
-      })
-    })();
-
-    const getCurrentProject = () => {
-      return Projects.getCurrentProject();
-    };
 
     const addTodo = (() => {
       $(".Add-Task button").click(() => {
@@ -34,9 +18,41 @@ const todoApp = (() => {
         let todo_descrip = $(".notes textarea").val();
         let Todo = ToDo(todo_title,todo_descrip);
         let curr_proj = getCurrentProject();
-        Projects.getProject(curr_proj).addTodo(todo_title,Todo);
+        getProjectsObj().getProject(curr_proj).addTodo(todo_title,Todo);
       })
     })()
+
+    const selectedProject = (projects) => {
+      projects.forEach((project) => {
+        project.onclick = (e) => {
+          let curr_proj = e.currentTarget.innerHTML;
+          getProjectsObj().setCurrentProject(curr_proj);
+          // for viewing todos of current project
+          let curr_proj_obj = getProjectsObj().getProject(curr_proj);
+          PubSub.publish("current project",curr_proj_obj);
+        }
+      })
+    }
+
+    const changeProject = (() => {
+      let projects = document.querySelectorAll(".project p");
+      PubSub.subscribe('project added', () => {
+        let projects = document.querySelectorAll(".project p");
+        selectedProject(projects);
+      })
+        selectedProject(projects);
+    })();
+
+
+    const getCurrentProject = () => {
+      return getProjectsObj().getCurrentProject();
+    };
+
+
+
+    const getProjectsObj = () => {
+      return Projects;
+    }
 
     return {};
 })();
